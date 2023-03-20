6a3d5d02ac676f57d616253d4d9f35aae033026e
Adler <peter.adler@usu.edu>
2023-03-20 13:52:18 -0600

    get daymet climate

---
+++ satellites/code/pull_climate_data.R
@@ -0,0 +1,69 @@
+
+library(daymetr)
+library(dplyr)
+library(tidyr)
+
+###
+### Pull daymet data for satellite sites
+###
+
+siteD <- read.csv("../rawdata/SiteInfo_2021-2022.csv",header=T)
+siteD <- siteD[,1:3]
+names(siteD) <- c("SiteCode" ,"Lat","Lon")
+
+# drop Lehnoff sites (no data)
+tmp <- grep("LEHN", siteD$SiteCode)
+siteD <- siteD[-tmp,]
+
+for(i in 1:nrow(siteD)){
+  tmp <- download_daymet(site = siteD$SiteCode[i],
+                          lat = siteD$Lat[i],
+                          lon = siteD$Lon[i],
+                          start = 1980,
+                          end = 2022,
+                          internal = TRUE)
+  tmp$data$SiteCode <- siteD$SiteCode[i]
+  
+  if(i==1){
+    climD <- tmp$data
+  }else{
+    climD <- rbind(climD,tmp$data)
+  }
+  
+}
+
+# move SiteCode to first column
+climD <- climD[,c(10,1:9)]
+
+# define climate year and climate day
+climD$climYr <- ifelse(climD$yday > 273, climD$year+1,climD$year )
+climD$climDay <- ifelse(climD$yday > 273, climD$yday-273,climD$yday+(365-273))
+
+# reorder columns
+climD <- climD[,c(1:3,11,12,4:10)]
+
+# rename columns
+names(climD)[6:12] <- c("daylength","prcp","radiation","swe","tmax","tmin","vp")
+
+# save daily data to file (~ 30 MB)
+# write.csv(climD,"../deriveddata/Satellites_daymet_daily.csv",row.names=F)
+
+###
+### aggregate to climate year
+###
+
+# only consider fall - spring
+Fa2SprD <- subset(climD,climD$climDay < 270)
+
+# calculate daily mean temperature
+Fa2SprD$tavg <- (Fa2SprD$tmax + Fa2SprD$tmin)/2
+
+annD <- Fa2SprD %>% group_by(SiteCode,climYr) %>%
+            summarise(prcp=sum(prcp),
+                      tmean=mean(tavg),
+                      swe_max=max(swe))
+
+# save annual data to file
+write.csv(annD,"../deriveddata/Satellites_daymet_Fall2Spr_means.csv",row.names=F)
+
+rm(climD,Fa2SprD,annD,siteD,tmp)
