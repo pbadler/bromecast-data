c5f731750eccde74313ce49c93e154faf42f0833
Adler <peter.adler@usu.edu>
2023-07-12 16:24:03 -0600

    cleaning new data

--- satellites/code/demography_prelim_analysis.R
+++ satellites/code/demography_prelim_analysis.R
@@ -94,12 +94,18 @@ D$Reproduced[D$Reproduced==""] <- "N"
 # fill NAs for "Reproduced" when we know fecundity > 0
 tmp <- which(is.na(D$Reproduced) & D$Fecundity > 0)
 D$Reproduced[tmp] <- "Y"
+# fill 0's for fecundity when Reproduced==No
+tmp <- which(is.na(D$Fecundity) & D$Reproduced == "N")
+D$Fecundity[tmp] <- 0
+
+# flag records where Reproduced = Y and Fecundity is NA
+D$fecundityflag <- ifelse(D$Reproduced=="Y" & is.na(D$Fecundity),1,0)
 
 # subset to complete observations
 D <- subset(D,!is.na(D$Fecundity))
 
 # remove and reorder columns
-D2021 <- D[,c("SiteCode","Year","Treatment","Transect","Distance","Emerged","Reproduced","Fecundity")]
+D2021 <- D[,c("SiteCode","Year","Treatment","Transect","Distance","Emerged","Reproduced","Fecundity","fecundityflag")]
 
 sapply(D2021, function(x) sum(is.na(x)))
 
@@ -123,36 +129,43 @@ names(D)[which(names(D)=="Distance.from.center..m.")] <- "Distance"
 
 D$Year <- 2022
 
-# fix bad values
+# fix bad values in Treatment column
 D$Treatment[D$Treatment=="removal "] <- "removal"
 D$Treatment[D$Treatment=="Removal"] <- "removal"
 D$Treatment[D$Treatment=="Control"] <- "control"
 
-D$Fecundity[D$Fecundity=="unk"] <- NA
-tmp <- which(D$Fecundity=="*seed head broke")
-D$Fecundity[tmp] <- NA
-D$Fecundity <- as.numeric(D$Fecundity)
+# fix bad values in Emerged column
+D$Emerged[D$Emerged=="-"] <- "missing"
+D$Emerged[D$Emerged=="?"] <- "missing"
+D$Emerged[D$Emerged=="No_skewer"] <- "missing"
+D$Emerged[D$Emerged=="No_skewer?"] <- "missing"
+D$Emerged[D$Emerged=="Unk"] <- "missing"
+D$Emerged[D$Emerged=="Y?"] <- "Y"
 
-# replace NAs with real zeros
-# TO DO: use notes column to find and retain "real" NAs (missing plants),
-# in this version, those are included as zeros
-D$Reproduced[is.na(D$Reproduced) & D$Emerged=="N"] <- "N"
-D$Fecundity[is.na(D$Fecundity) & D$Reproduced=="N"] <- 0
-
-# fix some bad values
+# fix some bad values in Reproduced column
+D$Reproduced[D$Emerged=="missing"] <- "missing"
 D$Reproduced[D$Reproduced=="dead"] <- "N"
 # fill NAs for "Reproduced" when we know fecundity > 0
 tmp <- which(is.na(D$Reproduced) & D$Fecundity > 0)
 D$Reproduced[tmp] <- "Y"
+D$Reproduced[is.na(D$Reproduced) & D$Emerged=="N"] <- "N"
+# assume remaining NAs are missing plants
+D$Reproduced[is.na(D$Reproduced)] <- "missing"
 
-# subset to complete observations
-D <- subset(D,!is.na(D$Fecundity))
+# fix bad values in Fecundity column
+# TO DO: use notes column to find and retain "real" NAs (missing plants),
+# in this version, these may be a mix of zeros and NAs
+D$Fecundity[D$Fecundity=="unk"] <- NA
+tmp <- which(D$Fecundity=="*seed head broke")
+D$Fecundity[tmp] <- NA
+D$Fecundity <- as.numeric(D$Fecundity)
+D$Fecundity[is.na(D$Fecundity) & D$Reproduced=="N"] <- 0
 
-# remove one more record with 0 for fecundity but NA for reproduced
-D <- subset(D,!is.na(D$Reproduced))
+# flag records where Reproduced = Y and Fecundity is NA
+D$fecundityflag <- ifelse(D$Reproduced=="Y" & is.na(D$Fecundity),1,0)
 
 # remove and reorder columns
-D2022 <- D[,c("SiteCode","Year","Treatment","Transect","Distance","Emerged","Reproduced","Fecundity")]
+D2022 <- D[,c("SiteCode","Year","Treatment","Transect","Distance","Emerged","Reproduced","Fecundity","fecundityflag")]
 
 sapply(D2022, function(x) sum(is.na(x)))
 
@@ -204,6 +217,9 @@ rm(siteD1)
 ### analyze probability of producing seeds
 ###
 
+# Get rid of missing records
+D <- subset(D,Reproduced!="missing")
+
 # convert N/Y to 0/1
 D$Reproduced <- as.numeric(as.factor(D$Reproduced))-1
 
@@ -231,7 +247,8 @@ summary(lm(x~1))
 ### model fecundity, conditional on seed production
 ###
 
-fecD <- subset(D,D$Fecundity>0)
+
+fecD <- subset(D,D$Fecundity>0 & D$fecundityflag==0)
 
 # # get counts by site year treatment
 # tmp <- fecD %>% group_by(SiteCode,Year,Treatment) %>%mutate(count=1) %>% 
