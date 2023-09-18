c01a33ab2a6c7ef90e34bbb47aaa35d0ff41a358
Vahsen <mlvahsen@gmail.com>
2023-09-18 10:35:41 -0400

    added wet degree day data and helper code for satellite sites

---
+++ modeling/code/CalculateGrowingDegreeDays.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(data.table)
+
+alldata = read_csv("modeling/data/AllSitesWPandTemp2.csv")
+data = subset(alldata, Year >2020) # First 1-2 years of data may not be as accurate. 
+#The model does not know starting conditions very well, so it takes a season or maybe 
+#two for the model to produce accurate estimates
+
+
+#calculate Growing Degrees ------ wet growing degrees = soil temperature when soil is wet (>-1.25 MPa)
+data$GrowingDegrees2cm = ifelse(data$WP2cm>-1.5 & data$Temp2cm > 0, data$Temp2cm,0)
+data$GrowingDegrees5cm = ifelse(data$WP5cm>-1.5 & data$Temp5cm > 0, data$Temp5cm,0)
+data$GrowingDegrees10cm = ifelse(data$WP10cm>-1.5 & data$Temp10cm > 0, data$Temp10cm,0)
+
+#Mark Wet Days ------ mark if a day has wet soil 
+data$GrowingDegreeDay2cm = ifelse(data$WP2cm>-1.5 & data$Temp2cm > 0, 1,0)
+data$GrowingDegreeDay5cm = ifelse(data$WP5cm>-1.5 & data$Temp5cm > 0, 1,0)
+data$GrowingDegreeDay10cm = ifelse(data$WP10cm>-1.5 & data$Temp10cm > 0, 1,0)
+
+
+MonthlySummary = data %>%
+  group_by(Site, Year, Month) %>%
+  summarize(Growing_degree_days2cm = sum(GrowingDegrees2cm),
+            Growing_degree_days5cm = sum(GrowingDegrees2cm),
+            Growing_degree_days10cm = sum(GrowingDegrees2cm),
+            Wet_days2cm = sum(GrowingDegrees2cm),
+            Wet_days5cm = sum(GrowingDegrees2cm),
+            Wet_days10cm = sum(GrowingDegrees2cm))
