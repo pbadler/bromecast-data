eefbd85fb5ad938496bb30ec16849ff48894ac28
mlvahsen <mlvahsen@gmail.com>
2024-04-29 10:21:51 -0400

    cleaning Cheyenne and Wildcat 2023 harvest and phenology data

---
+++ gardens/code/format_growthphenology_WY_2023.R
@@ -0,0 +1,180 @@
+# Formatting Cheyenne phenology to be in same structure as Sheep Station 2023
+
+library(lubridate); library(tidyverse); library(here)
+
+doyear <- 2023 # growing season to do
+dosite <- "CH" # code for focal site
+
+# Read in data
+julyB <- suppressMessages(read_csv("gardens/rawdata/Wy_Census_data2023_FINAL_July2.csv"))
+julyA <- suppressMessages(read_csv("gardens/rawdata/Wy_Census_data2023_FINAL_July1.csv"))
+juneB <- suppressMessages(read_csv("gardens/rawdata/Wy_Census_data2023_FINAL_June2.csv"))
+juneA <- suppressMessages(read_csv("gardens/rawdata/Wy_Census_data2023_FINAL_June1.csv"))
+mayB <- suppressMessages(read_csv("gardens/rawdata/Wy_Census_data2023_FINAL_May2.csv"))
+mayA <- suppressMessages(read_csv("gardens/rawdata/Wy_Census_data2023_FINAL_May1.csv"))
+aprB <- suppressMessages(read_csv("gardens/rawdata/Wy_Census_data2023_FINAL_April2.csv"))
+aprA <- suppressMessages(read_csv("gardens/rawdata/Wy_Census_data2023_FINAL_April1.csv"))
+
+# Create a list of data
+all_phen <- list(julyB, julyA, juneB, juneA, mayB, mayA, aprB, aprA)
+
+# Create vector of days (these are just placeholders for now)
+dates <- c("julyB", "julyA","juneB", "juneA", "mayB", "mayA", "aprB", "aprA")
+
+for(i in 1:length(dates)){
+  
+  # Set temp data name
+  rawD <- all_phen[[i]]
+  
+  # Fix alignment of column names
+  rawD_col_1_4 <- rawD[3:302,1:4]
+  names(rawD_col_1_4) <- tolower(rawD[2,1:4])
+  
+  # Fix issues with different locations of column headers -- plants
+  rawD_plants <- rawD[3:302,5:37]
+  names(rawD_plants) <- c("x", paste(rep(c("v", "notes"), 16), rep(1:16, each = 2), sep = "_"))
+  
+  # Bring two data sets back together
+  newD <- cbind(rawD_col_1_4, rawD_plants)
+  
+  # Bring v stage columns to rows
+  newD %>% 
+    gather(key = y, value = v, contains("v")) %>% 
+    mutate(y = as.numeric(parse_number(y))) %>%
+    dplyr::select(date, albedo, density, block, x, y, v) %>% 
+    distinct() -> newD_v
+    
+  # Bring notes columns to rows
+  newD %>% 
+    gather(key = y, value = notes, contains("notes")) %>% 
+    mutate(y = as.numeric(parse_number(y))) %>% 
+    dplyr::select(date, albedo, density, block, x, y, notes) %>% 
+    distinct() -> newD_notes
+  
+  cbind(newD_v, notes = newD_notes$notes)-> longD
+  
+  # Remove data for y = 9:16 for high density treatment
+  longD %>% 
+    filter(density == "high" & x %in% 1:10 & y %in% 1:8) -> longD_high
+  
+  # Remove data for x = 6:10 for low density treatment
+  longD %>% 
+    filter(density == "low" & x %in% 1:5 & y %in% 1:16) -> longD_low
+  
+  # Bring together total dataset
+  deriveD <- rbind(longD_high, longD_low)
+  deriveD$block <- as.numeric(deriveD$block)
+  
+  # Manually add in plot numbers
+  deriveD %>% 
+    group_by(albedo, density, block) %>% 
+    summarize() %>% 
+    ungroup() %>% 
+    arrange(block) %>% 
+    mutate(plot = rep(1:4, 10)) -> plot_info
+  
+  # Reorder and create unique identifier
+  merge(deriveD, plot_info) %>% 
+    arrange(block, plot, x, y) %>% 
+    mutate(plantID = paste(dosite, doyear, 1:nrow(deriveD), sep = "_"),
+           live = case_when(v == "VX" ~ "N",
+                            v == "H" ~ NA,
+                            v == "n/a" ~ NA,
+                            T ~ "Y"),
+           jday = yday(mdy(date))) %>% 
+    mutate(jday = ifelse(jday < 270, jday, jday-365)) %>% 
+    dplyr::select(plantID, density, gravel = albedo, block, plot, x, y, jday, live, v, notes) -> phen_sample
+  
+  assign(paste0(dates[i], sep = "_", "phen"), phen_sample)
+  
+}
+
+# Bring all data together
+rbind(aprA_phen, aprB_phen, mayA_phen, mayB_phen,
+      juneA_phen, juneB_phen, julyA_phen, julyB_phen) -> phen_WY
+
+# Switch x and y so that x = 1:10 or 1:20 and y = 1:10 or 1:5
+phen_WY %>% 
+  mutate(x_new = y,
+         y_new = x) %>% 
+  mutate(x = x_new,
+         y = y_new)-> phen_WY
+
+# Arrange by plant ID and see if things make sense
+phen_WY %>% 
+  arrange(plantID, jday)
+
+# Write notes csv to alter manually
+tmp <- phen_WY$notes
+tmp[tmp==""] <- NA
+tmp <- tmp[!is.na(tmp)]
+tmp <- unique(tmp,MARGIN=2)
+tmp <- data.frame(notes=tmp,action=NA)
+# write.csv(tmp,file=paste0(here("gardens/deriveddata/"),dosite,doyear,"_notes_actions.csv"),row.names=F)
+# STILL NEED TO DO THIS!
+
+# Create final columns
+phen_WY %>% 
+  mutate(herbivory = NA,
+         frost_heave = NA,
+         tillers = NA,
+         harvested = case_when(v == "H" ~ "Y",
+                               T ~ "N"),
+         v = case_when(# If VX, that means it is dead
+                       v == "VX" ~ NA,
+                       v == "n/a" ~ NA,
+                       # Flowering red is the same as flowering purple?
+                       v == "FR" ~ "FP",
+                       v == "IB" ~ "BS",
+                       v == "H" ~ NA,
+                       T ~ v)) %>% 
+  select(-x_new, -y_new) -> phen_WY_format
+
+# Set data frame to be same name format as Boise and SS
+pgD <- phen_WY_format
+
+# pull out and format phenology and growth data and notes for each plant
+pgD <- pgD[order(pgD$plantID,pgD$jday),] # sort by plantID then Julian day
+
+# check for bad "live" values
+table(pgD$live) # N Y
+#change live column to factor
+pgD$live<-as.factor(pgD$live)
+
+# check for bad "v" values
+table(pgD$v)  # Already fixed
+
+# compile notes
+tmp <- pgD$notes
+tmp[tmp==""] <- NA
+tmp <- tmp[!is.na(tmp)]
+tmp <- unique(tmp,MARGIN=2)
+tmp <- data.frame(notes=tmp,action=NA)
+# write.csv(tmp,file=paste0(here("gardens/deriveddata/"),dosite,doyear,"_notes_actions.csv"),row.names=F)
+# MLV updated on 29 June 2023
+
+# Order and select pgD columns
+pgD %>% 
+  select(plantID, jday, live, v, herbivory, frost_heave, harvested, notes) -> pgD
+
+# write pgD to file
+write.csv(pgD,file=paste0(here("gardens/deriveddata/"),dosite,doyear,"_growthphenology_by_plantID.csv"),row.names=F)
+
+# Write plantID key data to file
+genotype_info <- suppressMessages(read_csv("gardens/rawdata/WY_genotypes_2023.csv"))
+
+genotype_info %>% 
+  dplyr::select(block, density = density_long, gravel = albedo, source, growout = bulkYear_final,
+                x = X, y = Y, genotype = genotypeID) -> genotype_info
+
+merge(genotype_info, phen_WY) %>% 
+  mutate(site = dosite,
+         year = doyear,
+         genotype = parse_number(genotype)) %>% 
+  dplyr::select(plantID, site, year, block, plot, x, y, genotype, growout, density, gravel) %>% 
+  distinct() -> plant_key
+
+write.csv(plant_key,file=paste0(here("gardens/deriveddata/"),dosite,doyear,"_plantID.csv"),row.names=F)
+
+# Remove all objects but pgD
+rm(list=setdiff(ls(), "pgD"))
--- gardens/code/format_harvest_allSites_2023.R
+++ gardens/code/format_harvest_allSites_2023.R
@@ -133,55 +133,6 @@ phen_harvest_ss %>%
 phen_harvest_ss %>% 
   filter(live == "N" & harvest == "Y") # Just seems random
 
-# Make plots to see if there are patterns in data not matching up
-# png("~/Desktop/high_density_plots_ss.png", height = 10, width = 12, res = 300, units = "in")
-# phen_harvest_ss %>% 
-#   filter(density == "high") %>% 
-#   mutate(id = paste("Block", block, "Plot", plot, "-", albedo, sep = " ")) %>% 
-#   mutate(live_harvest_check =
-#            case_when(live == "N" & harvest == "Y" ~ "not alive @ phenology check,\n but harvested",
-#                      live == "Y" & harvest == "N"  ~ "alive @ phenology check,\n but not harvested",
-#                      T ~ "seems good")) %>% 
-#   ggplot(aes(x = x, y = y, color = live_harvest_check)) +
-#   geom_point(size = 4) +
-#   theme_classic(base_size = 16) +
-#   scale_x_continuous(breaks = 1:8, position = "top") +
-#   scale_x_continuous(sec.axis = dup_axis(), breaks = 1:8) +
-#   scale_y_reverse(breaks = 1:10) +
-#   scale_y_reverse(sec.axis = dup_axis(), breaks = 1:10) +
-#   theme(legend.position = "top") +
-#   facet_wrap(~id) +
-#   scale_color_manual(values = c("orange", "brown1", "gray47")) +
-#   labs(y = "y-coordinate",
-#        x = "x-coordinate",
-#        color = "issue")
-# dev.off()
-
-# Graphic for low density plots
-# png("~/Desktop/low_density_plots_ss.png", height = 11, width = 18, res = 300, units = "in")
-# phen_harvest_ss %>% 
-#   filter(density == "low") %>% 
-#   mutate(id = paste("Block", block, "Plot", plot, "-", albedo, sep = " ")) %>% 
-#   mutate(live_harvest_check =
-#            case_when(live == "N" & harvest == "Y" ~ "not alive @ phenology check,\n but harvested",
-#                      live == "Y" & harvest == "N"  ~ "alive @ phenology check,\n but not harvested",
-#                      T ~ "seems good")) %>% 
-#   ggplot(aes(x = x, y = y, color = live_harvest_check)) +
-#   geom_point(size = 4) +
-#   theme_classic(base_size = 16) +
-#   scale_x_continuous(breaks = 1:16, position = "top") +
-#   scale_x_continuous(sec.axis = dup_axis(), breaks = 1:18) +
-#   scale_y_reverse(breaks = 1:5) +
-#   scale_y_reverse(sec.axis = dup_axis(), breaks = 1:5) +
-#   theme(legend.position = "top") +
-#   facet_wrap(~id, nrow = 5) +
-#   scale_color_manual(values = c("orange", "gray47")) +
-#   labs(y = "y-coordinate",
-#        x = "x-coordinate",
-#        color = "issue")
-# dev.off()
-
-# Check to see how first flowering date matches up with fitness
 
 ## SS Fitness and flowering time ####
 # Merge phenology and harvest data sets to figure out which plants flowered
@@ -239,6 +190,9 @@ all_plants_no_flower %>%
 # Bring flowered and non-flowered plants back together
 rbind(all_plants_flowered, all_plants_no_flower) -> flower_fit
 
+# Write file to save in derived data sets
+write_csv(flower_fit, "gardens/deriveddata/SS2023_flower_fit.csv")
+
 # Group by genotype and get average fitness and flowering time
 flower_fit %>% 
   group_by(genotype) %>% 
@@ -252,9 +206,205 @@ flower_fit %>%
        x = "Mean first day of flowering")
 # Same pattern as we see in 2022, which is good!
 
+## Format Cheyenne harvest data ####
+# Subset to just Wildcat
+harvest %>%
+  filter(site == "Cheyenne") %>% 
+  mutate(site = "CH") -> harvest_ch
+
+# Check x and y coordinates
+table(harvest_ch$x)
+table(harvest_ch$y)
+# All good
+
+# Manually add in plot numbers
+harvest_ch %>% 
+  group_by(albedo, density, block) %>% 
+  summarize() %>% 
+  ungroup() %>% 
+  arrange(block) %>% 
+  mutate(plot = rep(1:4, 10)) -> plot_info
+merge(harvest_ch %>% select(-plot), plot_info) -> harvest_ch
+
+# Check block and plot ID
+table(harvest_ch$plot) # All good
+
+# Format dates
+harvest_ch %>% 
+  mutate(date = ifelse(date == "UNKNOWN", NA, date),
+         date = mdy(date)) -> harvest_ch
+
+# Create action list for notes
+# compile notes
+tmp <- harvest_ch$notes
+tmp[tmp==""] <- NA
+tmp <- tmp[!is.na(tmp)]
+tmp <- unique(tmp,MARGIN=2)
+tmp <- data.frame(notes=tmp,action=NA)
+# write.csv(tmp,file=paste0(here("gardens/deriveddata/"),"CH","2023","_harvest_notes.csv"),row.names=F)
+# Edited by MLV on 29 April 2024
+rm(tmp)
+
+# Bring notes back in and include standardized notes only
+notes_ch <- read_csv("gardens/deriveddata/CH2023_harvest_notes.csv")
+notes_ch %>% 
+  filter(action == "action") %>% 
+  merge(harvest_ch, all = T) %>% 
+  # Select only the columns that we want
+  select(site, date, block, plot, density, albedo, x, y, genotype, growout,
+         live, v, harvest, tillers, biomass_whole, inflor_mass, note_standard) %>% 
+  # Arrange by position
+  arrange(block, plot, density, albedo, x, y) %>% 
+  # Recode v stage (choose the most advanced stage if multiple v stages
+  # recorded)
+  mutate(v = case_when(v == "FB_FG" ~ "FB",
+                       v == "FP_FG" ~ "FP",
+                       v == "FG_FP_FB" ~ "FB",
+                       v == "FG_FB" ~ "FB",
+                       v == "FB_FP" ~ "FB",
+                       v == "FG_FP" ~ "FP",
+                       v == "FP_FB" ~ "FB",
+                       v == "FB_FG_FP" ~ "FB",
+                       v == "FG_FB_FP" ~ "FB",
+                       v == "FP_FG_FB" ~ "FB",
+                       v == "FB_FP_FG" ~ "FB",
+                       v == "FP_FB_FB" ~ "FB",
+                       v == "FP_FB_FG" ~ "FB",
+                       v == "FP_BS" ~ "FP",
+                       T ~ v)) -> harvest_ch
+
+table(harvest_ch$live)
+table(harvest_ch$v)
+
+# Set live plants to be live if a v stage was recorded
+harvest_ch$live <- ifelse(harvest_ch$v %in% c("BS", "FG", "FP", "FB"), "Y", "N")
+
+# Read in plant IDs
+plantID_ch <- read_csv("gardens/deriveddata/CH2023_plantID.csv")
+# Read in phenology data
+phen_ch <- read_csv("gardens/deriveddata/CH2023_growthphenology_by_plantID.csv")
+# Read in phenology standardized notes
+notes_phen_ch <- read_csv("gardens/deriveddata/CH2023_notes_actions.csv")
+
+# Merge together plantIDs and phenology
+merge(phen_ch, plantID_ch) %>% 
+  # Merge together with notes
+  merge(notes_phen_ch, all = T) %>% 
+  # Select only columns that we need
+  select(plantID, site, year, block, plot, x, y, genotype, jday, live, v, herbivory, frost_heave, notes) %>% 
+  # Rearrange by location
+  arrange(block, plot, x, y) -> ch_clean_phenology
+
+# Get last phenology check for each plant
+ch_clean_phenology %>% 
+  filter(live %in% c("N", "Y")) %>% 
+  group_by(block, plot, x, y) %>% 
+  slice_max(jday) -> ch_last_phen
+
+# Merge phenology data with harvest data
+harvest_ch %>% 
+  select(harvest_date = date, block, plot, density, albedo, x, y, live_harvest = live,
+         v_harvest =  v, harvest, tillers, biomass_whole, inflor_mass,
+         note_standard_harvest = note_standard) %>%  
+  merge(ch_last_phen) -> phen_harvest_ch
+
+# See how well phenology and harvest match up
+phen_harvest_ch %>% 
+  # If we recorded a weight, then it was definitely harvested
+  mutate(harvest = ifelse(is.na(biomass_whole), "N", "Y")) -> phen_harvest_ch
+
+# Check accuracy  
+phen_harvest_ch %>% 
+  group_by(live, harvest) %>% 
+  summarize(n = n())
+
+## Cheyenne fitness and flowering time ####
+# Merge phenology and harvest data sets to figure out which plants flowered
+harvest_ch %>% 
+  select(harvest_date = date, block, plot, density, albedo, x, y, live_harvest = live, v_harvest =  v, harvest, tillers, biomass_whole, inflor_mass,
+         note_standard_harvest = note_standard) %>% 
+  merge(ch_clean_phenology) -> merged_dat 
+
+merged_dat %>% 
+  filter(v %in% c("FG", "FP", "FB")) %>% 
+  group_by(plantID) %>% 
+  slice_min(jday) -> plants_flowered_phenology
+
+# Figure out which ones flowered *after* last phenology check
+`%notin%` <- Negate(`%in%`)
+
+merged_dat %>% 
+  filter(plantID %notin% plants_flowered_phenology$plantID) %>% 
+  filter(v_harvest %in% c("FG", "FP", "FB")) %>% 
+  group_by(plantID) %>% 
+  slice_max(jday) -> plants_flowered_harvest
+
+# If first time flowered when harvested, adjust v stage and date
+plants_flowered_harvest %>% 
+  mutate(jday = yday(harvest_date),
+         jday = ifelse(jday < 270, jday, jday-365),
+         v = v_harvest) -> plants_flowered_harvest
+
+# Bring datasets back together
+rbind(plants_flowered_phenology, plants_flowered_harvest) -> all_plants_flowered
+
+# Get plants that did not flower
+merged_dat %>% 
+  filter(plantID %notin% all_plants_flowered$plantID) %>% 
+  group_by(plantID) %>% 
+  slice_min(jday) -> all_plants_no_flower
+
+# Get average flowering time of genotypes
+all_plants_flowered %>% 
+  group_by(genotype) %>% 
+  summarize(mean_flower = mean(jday)) %>% 
+  ungroup() -> genotype_averages
+
+# Assign plants that didn't flower the average flowering time for genotype
+all_plants_no_flower %>% 
+  merge(genotype_averages) %>% 
+  mutate(jday = mean_flower) %>% 
+  # And then assign fitness (inflor_mass) to be 0
+  mutate(inflor_mass = 0) -> all_plants_no_flower
+
+# Bring flowered and non-flowered plants back together
+rbind(all_plants_flowered, all_plants_no_flower) -> flower_fit
+
+
+# Remove plants with bad notes
+# flower_fit %>% 
+#   filter(note_standard %notin% c("bad_position", "duplicate", "no_date",
+#                                  "physical_damage", "smut", "seed_drop")) -> flower_fit_clean
+
+# Filter so we are only have plants with recorded harvest dates
+flower_fit %>% 
+  filter(complete.cases(jday)) -> flower_fit
+
+# Replace inflor_mass NA to be 0
+flower_fit$inflor_mass <- ifelse(is.na(flower_fit$inflor_mass), 0, flower_fit$inflor_mass)
+
+# Fix outlier point
+flower_fit[flower_fit$inflor_mass > 100,"inflor_mass"] <- 4.72
+
+# Write file to save in derived data sets
+write_csv(flower_fit, "gardens/deriveddata/CH2023_flower_fit.csv")
+
+# Group by genotype and get average fitness and flowering time
+flower_fit %>% 
+  group_by(genotype) %>% 
+  summarize(jday_avg = mean(jday),
+            fitness_avg = mean(inflor_mass)) %>% 
+  ggplot(aes(x = jday_avg, fitness_avg)) + 
+  geom_point() +
+  # Maybe a quadratic fit here?
+  geom_smooth(method = "lm", formula = y ~ x + I(x^2)) +
+  geom_smooth(method = "lm", color = "red") + 
+  theme_bw(base_size = 16) + 
+  labs(y = "Mean inflorescence mass (g)",
+       x = "Mean first day of flowering")
 
 ## Format WI harvest data ####
-# Subset to just Sheep Station
+# Subset to just Wildcat
 harvest %>%
   filter(site == "BoiseLow") %>% 
   mutate(site = "WI") -> harvest_wi
@@ -319,7 +469,7 @@ table(harvest_wi$live)
 # Set live plants to be live if a v stage was recorded
 harvest_wi$live <- ifelse(harvest_wi$v %in% c("BS", "FG", "FP", "FB"), "Y", "N")
 
-
+## Bring in phenology WI ####
 # Read in plant IDs
 plantID_wi <- read_csv("gardens/deriveddata/Boise2023_plantID.csv")
 # Read in phenology data
@@ -335,28 +485,6 @@ merge(phen_wi, plantID_wi) %>%
   select(plantID, site, year, block, plot, x, y, genotype, jday, live, v, herbivory, frost_heave, note_standard) %>% 
   # Rearrange by location
   arrange(block, plot, x, y) -> wi_clean_phenology
-
-# Get last phenology check for each plant
-wi_clean_phenology %>% 
-  group_by(block, plot, x, y) %>% 
-  slice_max(jday) -> wi_last_phen
-
-# Merge phenology data with harvest data
-harvest_wi %>% 
-  select(harvest_date = date, block, plot, density, albedo, x, y, live_harvest = live,
-         v_harvest =  v, harvest, tillers, biomass_whole, inflor_mass,
-         note_standard_harvest = note_standard) %>%  
-  merge(wi_last_phen) -> phen_harvest_wi
-
-# See how well phenology and harvest match up
-phen_harvest_wi %>% 
-  # If we recorded a weight, then it was definitely harvested
-  mutate(harvest = ifelse(is.na(biomass_whole), "N", "Y")) -> phen_harvest_wi
-
-# Check accuracy  
-phen_harvest_wi %>% 
-  group_by(live, harvest) %>% 
-  summarize(n = n())
 
 ## WI Fitness and flowering time ####
 # Merge phenology and harvest data sets to figure out which plants flowered
@@ -422,6 +550,9 @@ flower_fit_clean %>%
 # Replace inflor_mass NA to be 0
 flower_fit_clean$inflor_mass <- ifelse(is.na(flower_fit_clean$inflor_mass), 0, flower_fit_clean$inflor_mass)
 
+# Write csv of flower fitness data
+write_csv(flower_fit_clean, "gardens/deriveddata/Boise2023_flower_fit.csv")
+
 # Group by genotype and get average fitness and flowering time
 flower_fit_clean %>% 
   group_by(genotype) %>% 
@@ -435,27 +566,3 @@ flower_fit_clean %>%
        x = "Mean first day of flowering")
 # Same pattern as we see in 2022, which is good!
 
-# Make plot for SS and WI 
-flower_fit %>% 
-  group_by(genotype) %>% 
-  summarize(jday_avg = mean(jday),
-            fitness_avg = mean(inflor_mass)) %>% 
-  ungroup() %>% 
-  mutate(site = "SS") -> ss_to_plot
-
-flower_fit_clean %>% 
-  group_by(genotype) %>% 
-  summarize(jday_avg = mean(jday),
-            fitness_avg = mean(inflor_mass)) %>% 
-  ungroup() %>% 
-  mutate(site = "WI") -> wi_to_plot
-
-png("~/Desktop/fitflower_2023.png", height = 4, width = 6, res = 300, units = "in")
-rbind(ss_to_plot, wi_to_plot) %>% 
-  ggplot(aes(x = jday_avg, fitness_avg, color = site)) + 
-  geom_point() +
-  geom_smooth(method = "lm") +
-  theme_bw(base_size = 16) + 
-  labs(y = "Mean inflorescence mass (g)",
-       x = "Mean first day of flowering")
-dev.off()  
